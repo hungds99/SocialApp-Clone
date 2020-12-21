5304df7303845ae28275678018b804f32777cca4
nguyenquan28 <nguyenquan.cntt.k17da@gmail.com>
2020-12-21 16:58:46 +0700

    Quân đẹp trai đã khai code giờ đẹp.

---
+++ src/component/login/Login.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Login extends Component {
+    render() {
+        return (
+            <div>
+                <p>Quân agusta đã ghé qua đây</p>
+            </div>
+        );
+    }
+}
+
+export default Login;
