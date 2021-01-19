95d84e6621aa77f5e72d4f7daa6d969c9ce94174
VoVanThanh1999 <mrt.boss@yahoo.com>
2021-01-19 13:36:21 +0700

    thanh update profile

--- src/App.js
+++ src/App.js
@@ -1,15 +1,18 @@
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+import Individual from './component/Profile/Individual'
 import { RouteLink } from './constants'
 import Home from './pages/home'
 import SignUp from './pages/signUp'
 
 function App() {
+
     return (
         <>
             <Router>
                 <Switch>
                     <Route path={RouteLink.home} component={Home} exact />
                     <Route path={RouteLink.signUp} component={SignUp} />
+                    <Route path={RouteLink.profile} exact component={Individual} />
                 </Switch>
             </Router>
         </>
